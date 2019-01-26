5c8cc009fcfabbe6a3c627a94f3ed65e32368028
Berlian Khatulistiwa <berlink.k2@gmail.com>
2019-01-26 02:33:54 +0000

    Finished Chapter 6 + Chapter 6 exercises

--- rango/admin.py
+++ rango/admin.py
@@ -4,9 +4,14 @@ from rango.models import Category, Page
 # Register your models here.
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'views', 'likes')
+    prepopulated_fields = {'slug': ('name',)}
+
+
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from rango.models import Category
+from rango.models import Category, Page
 from django.http import HttpResponse
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+
+    except Category.DoesnotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+
+    return render(request, 'rango/category.html', context_dict)
 
 
 def index(request):
@@ -10,7 +29,8 @@ def index(request):
     # Place the list in our context_dict dictionary
     # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    pages_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': pages_list}
     # Render the response and send it back!
     return render(request, 'rango/index.html', context_dict)
 
