6d53aa9d7ed4bc54419c60254926ba4827cacf94
Berlian Khatulistiwa <berlink.k2@gmail.com>
2019-02-01 11:16:21 +0000

    Chapter 10 totally finished

--- rango/views.py
+++ rango/views.py
@@ -8,6 +8,7 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from datetime import datetime
 
 
 @login_required
@@ -79,19 +80,61 @@ def index(request):
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary
     # that will be passed to the template engine.
+    request.session.set_test_cookie()
     category_list = Category.objects.order_by('-likes')[:5]
     pages_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': pages_list}
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/index.html', context = context_dict)
     # Render the response and send it back!
-    return render(request, 'rango/index.html', context_dict)
+    return response
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    # If it has been more than a day since the last visit
+    if(datetime.now() - last_visit_time).days > 0:
+        visits = visits +1
+        # Update last visit cookie since we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    # Update/sets the visit cookie
+    request.session['visits'] = visits
 
 
 def about(request):
+    if request.session.test_cookie_worked():
+        print("Test cookie worked")
+        request.session.delete_test_cookie()
     # Prints out whether message is a GET or a POST
     print(request.method)
     # Prints out the user name, if no one is logged in it prints anonymoususer
     print(request.user)
-    return render(request, 'rango/about.html', {})
+
+    context = RequestContext(request)
+    context_dict = {}
+    count = request.session.get('visits', 0)
+    context_dict['visit_count'] = count
+    return render_to_response('rango/about.html', context_dict, context)
 
 
 def register(request):
